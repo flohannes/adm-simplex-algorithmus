b8d7d8c7687d6441fd5f47bdaac2f4be2434c6ab
rene.wetzig@gmail.com <rene.wetzig@gmail.com@48600afd-661e-811c-b394-3ed6fbf698c2>
2013-01-28 22:58:49 +0000

    Einlesen der Befehle durch die Kommandozeile

--- Programmieraufgabe/src/de/berlin/tuberlin/adm/main/netzwerksimplex.java
+++ Programmieraufgabe/src/de/berlin/tuberlin/adm/main/netzwerksimplex.java
@@ -16,21 +16,24 @@ public class netzwerksimplex {
 	public static void main(String[] args) throws IOException {
 		
 		//args[0] ist Eingabedatei
-		//mit Input sollen die Daten eingelesen werden und ein Graph zurück gegeben werden
-		Input input = new Input("src/InputData/big7.net");
-		System.out.println("Time in ms: " + input.getStopwatch().getElapsedTime());
+		//mit Input sollen die Daten eingelesen werden und ein Graph zurueck gegeben werden
+//		Input input = new Input("src/InputData/big7.net");
+		Input input = new Input(args[0]);
+//		System.out.println("Time in ms: " + input.getStopwatch().getElapsedTime());
 
 		
-		//Ausführen von der Klasse SimplexAlgorithmus. ein Graph soll eingegeben werden und berechnet werden.
-		System.out.println(input.getGraph().toString());
+		//Ausfuehren von der Klasse SimplexAlgorithmus. ein Graph soll eingegeben werden und berechnet werden.
+//		System.out.println(input.getGraph().toString());
 		SimplexAlgorithm simplex = new SimplexAlgorithm(input.getGraph());
 		simplex.startOptimierung();
 //		System.out.println(input.getGraph().toString());
+		System.out.println("Kosten insgesamt: "+ simplex.calculateObjective());
 
 		System.out.println("Time in ms: " + simplex.getStopwatch().getElapsedTime());
 
 		//args[1] ist Ausgabedatei und bekommt von SimplexAlgorithmus das Ergebnis, welches in eine Ausgabedatei geschrieben werden soll.
-		Output output = new Output("src/OutputData", simplex.getGraph(), simplex.calculateObjective());
+//		Output output = new Output("src/OutputData", simplex.getGraph(), simplex.calculateObjective());
+		Output output = new Output(args[1], simplex.getGraph(), simplex.calculateObjective());
 	}
 
 }
